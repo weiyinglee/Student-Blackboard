287b98c5d98a9af637fbd7c74b69080d509dfe29
WeiYing Lee <weiyinglee@cpp.edu>
2016-11-04 01:55:30 -0700

    add more routes for other tabs under main, rendering with same navbar and footer

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,33 @@
 class MainController < ApplicationController
+	#check before rendering action
+	before_action :check_login, only: [:index, :schedule, :note, :calculator]
+
 	def index
-		if session[:user_id] == nil
-			redirect_to '/'
-		end
+		@user = load_user
 	end
+
+	def schedule
+		@user = load_user
+	end
+
+	def note
+		@user = load_user
+	end
+
+	def calculator
+		@user = load_user
+	end
+
+	private
+		#check if the user logged in
+		def check_login
+			if session[:user_id] == nil
+				redirect_to '/'
+			end
+		end
+
+		#load the user info
+		def load_user
+			User.find_by_id(session[:user_id])
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 	get "/register", to: "register#index"
 	get "/main", to: "main#index", as: "main"
+	get "/main/class_schedule", to: "main#schedule", as: "schedule"
+	get "/main/personal_note", to: "main#note", as: "note"
+	get "/main/gpa_calculator", to: "main#calculator", as: "calculator"
+
 	post "/register", to: "register#create"
 	post "/", to: 'login#create'
+	
 	get "/logout", to: "login#destroy"
 
 	root "login#index"
